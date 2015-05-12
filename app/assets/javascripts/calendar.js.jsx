f3f374b01dbb3879ebfa23bed17590bae5a5d80f
ozaki <fs65ugumori@yahoo.co.jp>
2015-05-12 14:31:52 +0900

    Reactでカレンダー完成

--- app/assets/javascripts/calendar.js.jsx
+++ app/assets/javascripts/calendar.js.jsx
@@ -1,67 +1,52 @@
 this.Calendar = React.createClass({
+  getInitialState: function() {
+    return {
+      baseDate: moment(new Date())
+    };
+  },
+  today: function() {
+    this.setState({ baseDate: moment(new Date()) });
+  },
+  prevMonth: function() {
+    this.setState({ baseDate: this.state.baseDate.clone().add(-1, "month") });
+  },
+  nextMonth: function() {
+    this.setState({ baseDate: this.state.baseDate.clone().add(1, "month") });
+  },
   render: function() {
-    var baseDate = moment(new Date());
     return (
 <div className="container">
-  <CalendarTitle baseDate={baseDate}/>
-  <CalendarMonth baseDate={baseDate}/>
+  <div>
+    <button className="btn btn-default today" type="button" onClick={this.today}>今月</button>
+    <button className="btn btn-default prev" type="button" onClick={this.prevMonth}>&lt;</button>
+    <button className="btn btn-default next" type="button" onClick={this.nextMonth}>&gt;</button>
+    <span>{this.state.baseDate.clone().format('YYYY年M月')}</span>
+  </div>
+  <CalendarMonth baseDate={this.state.baseDate} />
 </div>
-    )
-  }
-});
-
-this.CalendarTitle = React.createClass({
-  render: function() {
-    return(
-<table>
-  <tbody>
-    <tr>
-      <td>
-        <button className="btn btn-default today" type="button">今月</button>
-      </td>
-      <td>
-        <button className="btn btn-default prev" type="button">&lt;</button>
-      </td>
-      <td>
-        <button className="btn btn-default next" type="button">&gt;</button>
-      </td>
-      <td>
-        <h4>{this.props.baseDate.clone().format('YYYY年M月')}</h4>
-      </td>
-    </tr>
-  </tbody>
-</table>
     )
   }
 });
 
 this.CalendarMonth = React.createClass({
   render: function() {
+    var weekdays = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"]
     return (
 <table className="table table-bordered">
-  <CalendarHeader/>
-  <CalendarBody baseDate={this.props.baseDate}/>
+  <thead>
+    <tr className="calendar__weekday-header">
+      {weekdays.map(function(weekday, i) {
+        return <th key={i} className="calendar__weekday">{weekday}</th>;
+      })}
+    </tr>
+  </thead>
+  <CalendarDay baseDate={this.props.baseDate} />
 </table>
     )
   }
 });
 
-this.CalendarHeader = React.createClass({
-  render: function() {
-    var weekdays = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"]
-    return (
-<thead>
-  <tr className="calendar__weekday-header">
-    {weekdays.map(function(weekday, i) {
-      return <th key={i} className="calendar__weekday">{weekday}</th>;
-    })}
-  </tr>
-</thead>
-    )
-  }
-});
-
-this.CalendarBody = React.createClass({
+this.CalendarDay = React.createClass({
   render: function() {
     var baseDate = this.props.baseDate;
     var startDate = baseDate.clone().startOf("month").day(0);
@@ -70,12 +55,11 @@ this.CalendarBody = React.createClass({
     var days = [];
 
     _.times(totalDays, function(n) {
-      var day = startDate.clone().add("days", n);
-      var classes = day.format("M") == baseDate.format("M") ? "calendar__date" : "calendar__out-of-date"
-      days.push(<td className={classes}>{day.format("D")}</td>);
+      var day = startDate.clone().add(n, "days");
+      var className = day.format("M") == baseDate.format("M") ? "calendar__date" : "calendar__out-of-date";
+      days.push(<td key={n} className={className}>{day.format("M/D")}</td>);
     });
-    var weeks = _.chunk(days, 7)
-        
+    var weeks = _.chunk(days, 7);
     return(
       <tbody>
         {weeks.map(function(week, i) {
