1ec7da8b8227b832a7d6589d25945e87f469ed4e
ozaki <fs65ugumori@yahoo.co.jp>
2015-05-19 19:07:20 +0900

    seedでデータを作成できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+License.create(name: "100VU", color: "#80CBC4").tap do |license|
+  (1..9).each do |n|
+    license.lending_histories.create!(date: "2015051#{n}")
+  end
+end
+
+License.create(name: "250VU", color: "#42A5F5").tap do |license|
+  (1..9).each do |n|
+    license.lending_histories.create!(date: "2015051#{n}")
+  end
+end
+
+License.create(name: "500VU", color: "#7E57C2").tap do |license|
+  (1..9).each do |n|
+    license.lending_histories.create!(date: "2015051#{n}")
+  end
+end
+
+License.create(name: "1000VU", color: "#EF5350").tap do |license|
+  (1..9).each do |n|
+    license.lending_histories.create!(date: "2015051#{n}")
+  end
+end
