8c810b6ae6c29334b6739303485fcc1d538931bc
ozaki <fs65ugumori@yahoo.co.jp>
2015-06-02 14:20:35 +0900

    requestモデルにvalidateを追加

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,10 @@ class Request < ActiveRecord::Base
   extend Enumerize
   has_many :lending_histories, dependent: :destroy
   enumerize :status, in: [:approved, :waiting], scope: true, default: :waiting
+  validates :date, presence: true
+  validates :name, presence: true
+  validates :department, presence: true
+  validates :controller, presence: true
+  validates :division, presence: true
+  validates :block, presence: true
 end
